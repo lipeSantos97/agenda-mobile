75d193b52b9de70822edb28b375f159240832dfa
Luiz Santos <lfp.santos543@gmail.com>
2024-10-15 22:06:27 -0300

    criação do component button

---
+++ src/components/Button/button.style.js
@@ -0,0 +1,14 @@
+export const styles = {
+  btn: {
+    width: "100%",
+    backgroundColor: "#0D6EFD",
+    borderRadius: 6,
+    padding: 12,
+  },
+
+  text: {
+    color: "#fff",
+    fontSize: 15,
+    textAlign: "center",
+  },
+}
---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+const COLORS = {
+  red: "#DF5951",
+  blue: "0D6EFD",
+  white: "#ffffff"
+}
+
+const FONT_SIZE = {}
+
+export { COLORS, FONT_SIZE }
